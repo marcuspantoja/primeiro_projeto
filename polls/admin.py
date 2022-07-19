8e5410348f60661f5c23343bc94f8cdc4001adc0
Marcus Pantoja <marcuspantoja@MacBook-Air-de-Marcus.local>
2022-07-19 08:32:39 -0300

    segundo commit

--- polls/admin.py
+++ polls/admin.py
@@ -4,3 +4,5 @@ from django.contrib import admin
 from .models import Poll
 
 admin.site.register(Poll)
+
+
--- primeiro_projeto/wsgi.py
+++ primeiro_projeto/wsgi.py
@@ -4,6 +4,7 @@ WSGI config for primeiro_projeto project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
